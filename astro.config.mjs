df66a6199cb6b292b53a87a10bb14b33048ea54f
TechStreet <dftechstreet@gmail.com>
2024-05-04 17:17:50 +0100

    add: standalone nodejs adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+	output: "server",
+	adapter: node({
+	  	mode: "standalone"
+	}),
 	integrations: [
 		starlight({
 			title: 'Knowledgebase',
